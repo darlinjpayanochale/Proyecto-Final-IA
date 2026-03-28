30e06610144980a336f156419421adee7df35d7c
Darlin Javier Payano Chale <darlinjpayanochale@gmail.com>
2026-03-28 19:04:43 -0400

    optimización para ejecución en CPU

--- detector_objetos_peligrosos/main.py
+++ detector_objetos_peligrosos/main.py
@@ -1,48 +1,51 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 cap = cv2.VideoCapture(0)
 
-# Lista de objetos peligrosos
-dangerous_objects = ["knife", "scissors"]
+dangerous_objects = ["knife", "scissors", "fork", "spoon"]
+
+frame_count = 0
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    results = model(frame)
+    frame_count += 1
 
-    detected_danger = False
+    if frame_count % 3 == 0:
+        results = model(frame, imgsz=320, conf=0.25)
 
-    # Revisar detecciones
-    for r in results:
-        for box in r.boxes:
-            cls = int(box.cls[0])
-            label = model.names[cls]
+        detected_danger = False
 
-            if label in dangerous_objects:
-                detected_danger = True
+        for r in results:
+            for box in r.boxes:
+                cls = int(box.cls[0])
+                label = model.names[cls]
 
-    annotated_frame = results[0].plot()
+                print("Detectado:", label)
 
-    # Mostrar alerta
-    if detected_danger:
-        cv2.putText(
-            annotated_frame,
-            "PELIGRO DETECTADO",
-            (50, 50),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 0, 255),
-            3
-        )
+                if label in dangerous_objects:
+                    detected_danger = True
 
-    # Mostrar ventana (SIEMPRE)
-    cv2.imshow("Detector IA", annotated_frame)
+        annotated_frame = results[0].plot()
 
-    # Salir con Q
+        if detected_danger:
+            cv2.putText(
+                annotated_frame,
+                "PELIGRO DETECTADO",
+                (50, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 0, 255),
+                3
+            )
+
+        cv2.imshow("Detector IA", annotated_frame)
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
