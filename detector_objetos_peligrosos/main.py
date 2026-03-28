266c6898a1e6ef89b7cb72a8ac7a9846f8ead004
Darlin Javier Payano Chale <darlinjpayanochale@gmail.com>
2026-03-28 18:24:18 -0400

    agregada deteccion de objetos peligrosos con alerta visual

--- detector_objetos_peligrosos/main.py
+++ detector_objetos_peligrosos/main.py
@@ -4,16 +4,45 @@ import cv2
 model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture(0)
 
+# Lista de objetos peligrosos
+dangerous_objects = ["knife", "scissors"]
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
     results = model(frame)
+
+    detected_danger = False
+
+    # Revisar detecciones
+    for r in results:
+        for box in r.boxes:
+            cls = int(box.cls[0])
+            label = model.names[cls]
+
+            if label in dangerous_objects:
+                detected_danger = True
+
     annotated_frame = results[0].plot()
 
+    # Mostrar alerta
+    if detected_danger:
+        cv2.putText(
+            annotated_frame,
+            "PELIGRO DETECTADO",
+            (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            3
+        )
+
+    # Mostrar ventana (SIEMPRE)
     cv2.imshow("Detector IA", annotated_frame)
 
+    # Salir con Q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
